fd422d4bf6ee091c7e0e6044f1ac6f094ec1cf7a
Stefano Torresi <webdev@stefanotorresi.it>
2014-11-05 18:35:14 +0100

    update adapter factory

--- src/Storage/DataMapperAdapterFactory.php
+++ src/Storage/DataMapperAdapterFactory.php
@@ -8,7 +8,7 @@
 namespace Thorr\OAuth2\Storage;
 
 use Thorr\OAuth2\Repository;
-use Thorr\Persistence\Repository\Manager\RepositoryManager;
+use Thorr\Persistence\DataMapper\Manager\DataMapperManager;
 use Zend\Crypt\Password\Bcrypt;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
@@ -18,18 +18,18 @@ class DataMapperAdapterFactory implements FactoryInterface
     /**
      * Create service
      *
-     * @param ServiceLocatorInterface $serviceLocator
+     * @param  ServiceLocatorInterface $serviceLocator
      * @return DataMapperAdapter
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        /** @var RepositoryManager $repositoryManager */
-        $repositoryManager = $serviceLocator->get('Thorr\Persistence\Repository\Manager\RepositoryManager');
+        /** @var DataMapperManager $dataMapperManager */
+        $dataMapperManager = $serviceLocator->get('Thorr\Persistence\Repository\Manager\RepositoryManager');
 
         /** @var Bcrypt $bcrypt */
         $password = $serviceLocator->get('Thorr\OAuth2\Password\Bcrypt');
 
-        $adapter = new DataMapperAdapter($password, $repositoryManager);
+        $adapter = new DataMapperAdapter($password, $dataMapperManager);
 
         return $adapter;
     }
