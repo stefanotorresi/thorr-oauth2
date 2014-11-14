4aaa228e6ebdabfc30b27bf9d99030ba53446d97
Stefano Torresi <webdev@stefanotorresi.it>
2014-11-14 23:17:02 +0100

    add third party credentials data mapper interface

---
+++ src/DataMapper/ThirdPartyMapperInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Stefano Torresi (http://stefanotorresi.it)
+ * @license See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace Thorr\OAuth2\DataMapper;
+
+use Thorr\OAuth2\Entity\ThirdParty;
+use Thorr\OAuth2\GrantType\ThirdParty\Provider\ProviderInterface;
+use Thorr\Persistence\DataMapper\DataMapperInterface;
+
+interface ThirdPartyMapperInterface extends DataMapperInterface
+{
+    /**
+     * @param  ProviderInterface $provider
+     * @return ThirdParty|null
+     */
+    public function findByProvider(ProviderInterface $provider);
+}
