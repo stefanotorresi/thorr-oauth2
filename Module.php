ef220edb4fc7dca24911879385317751a4bccef3
Stefano Torresi <webdev@stefanotorresi.it>
2014-11-24 18:08:29 +0100

    move zf2 module config to Module.php

--- Module.php
+++ Module.php
@@ -12,12 +12,61 @@ use Zend\ModuleManager\Feature;
 class Module implements Feature\ConfigProviderInterface
 {
     /**
-     * Returns configuration to merge with application configuration
-     *
-     * @return array|\Traversable
+     * {@inheritdoc}
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return [
+            'thorr_oauth' => [
+                //        'user_entity_class_name' => Entity\User::class,
+                //        'bcrypt_cost' => 10,
+                //        'default_user_mapping_enabled' => true,
+                //        'third_party_grant_type_enabled' => false,
+                'third_party_providers' => [
+                    'facebook' => [
+                        'class' => GrantType\ThirdParty\Provider\FacebookProvider::class,
+                        'options' => [
+                            'app_id' => null,
+                            'uri' => 'https://graph.facebook.com/v2.0',
+                            'endpoint_params' => [],
+                        ],
+                    ],
+                    'instagram' => [
+                        'class' => GrantType\ThirdParty\Provider\InstagramProvider::class,
+                        'options' => [
+                            'client_id' => null,
+                            'uri' => 'https://api.instagram.com/v1'
+                        ],
+                    ],
+                ],
+            ],
+
+            'service_manager' => [
+                'factories' => [
+                    Options\ModuleOptions::class         => Options\ModuleOptionsFactory::class,
+                    Storage\DataMapperAdapter::class     => Storage\DataMapperAdapterFactory::class,
+                    GrantType\ThirdPartyGrantType::class => GrantType\ThirdParty\GrantTypeFactory::class,
+                ],
+                'delegators' => [
+                    'ZF\OAuth2\Service\OAuth2Server' => [
+                        Server\ServerInitializer::class,
+                    ],
+                ],
+            ],
+
+            'router' => [
+                'routes' => [
+                    'oauth' => [
+                        'options' => [
+                            'route' => '/api/oauth',
+                        ],
+                    ],
+                ],
+            ],
+
+            'zf-oauth2' => [
+                'storage' => Storage\DataMapperAdapter::class,
+            ],
+        ];
     }
 }
